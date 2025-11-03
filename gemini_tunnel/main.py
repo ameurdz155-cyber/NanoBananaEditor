08a027ee8e8b119275e4b6c3efe34c83b1f9af8a
Your Name <your@email.com>
2025-11-04 00:35:47 +0300

    feat: Add aipod-lite origin to allowed origins for CORS

--- gemini_tunnel/main.py
+++ gemini_tunnel/main.py
@@ -40,6 +40,7 @@ DEFAULT_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:4173",
     "http://127.0.0.1:4173",
+    "https://aipod-lite.findapply.com",
 ]
 
 raw_origins = os.getenv("ALLOWED_ORIGINS", "")
